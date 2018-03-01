324cb2caa3cacad281dcae05d6e2954ca9a30a79
Fullm3tal <pjacob@vt.edu>
2018-02-28 20:14:49 -0500

    Homework 2 is now uploaded 2/28/18.

---
+++ Homework02/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#include "functions.h"
+
+int main (int argc, char **argv) {
+
+	//seed value for the randomizer 
+  double seed;
+  
+  seed = clock(); //this will make your program run differently everytime
+  //seed = 0; //uncomment this and you program will behave the same everytime it's run
+  
+  srand48(seed);
+
+
+  //begin by getting user's input
+	unsigned int n;
+
+  printf("Enter a number of bits: ");
+  scanf("%u",&n);
+
+  //make sure the input makes sense
+  if ((n<2)||(n>30)) {
+  	printf("Unsupported bit size.\n");
+		return 0;  	
+  }
+ unsigned int p, q;
+p = pow(2, n)-1;
+printf("P is %d\n", p);
+
+q =(p-1);
+printf("Q is %d\n", q);
+  /* Q2.2: Use isProbablyPrime and randomXbitInt to find a random n-bit prime number */
+if (isProbablyPrime(randXbitInt(n))) 
+{
+ printf("%d is Prime\n", p);
+}
+  /* Q3.2: Use isProbablyPrime and randomXbitInt to find a new random n-bit prime number 
+     which satisfies p=2*q+1 where q is also prime */ 
+p = 2*q+1;
+if (isProbablyPrime(q) && (p < pow(2,n)&& p > pow(2, n)-1))
+{ 
+	printf("p = %u is probably prime and equals 2*q + 1. q= %u and is also probably prime.\n", p, q);  
+}	unsigned int g;
+	/* Q3.3: use the fact that p=2*q+1 to quickly find a generator */
+	g = findGenerator(p);
+	//printf("%u is a generator of Z_%d\n", g, p);
+  return 0;
+}
