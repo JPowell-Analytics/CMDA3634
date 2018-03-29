7a80688303fb88fc04d4d1843410b3f5f215d1e7
Fullm3tal <pjacob@vt.edu>
2018-03-29 16:04:24 -0400

    Updating HW03 Files

--- HW03/Part2/main.c
+++ HW03/Part2/main.c
@@ -35,25 +35,29 @@ int main (int argc, char **argv) {
     return 0;   
   }
   printf("\n");
+
   }
   //declare storage for an ElGamal cryptosytem
   unsigned int p, g, h, x;
- // startTime = MPI_Wtime();
+  if (rank == 0){
+  setupElGamal(n, &p, &g, &h, &x);
+ } 
+  // startTime = MPI_Wtime();
   //setup an ElGamal cryptosystem
-  if (rank == 0) {
-      setupElGamal(n,&p,&g,&h,&x);
       MPI_Bcast(&p, 1, MPI_INT, 0, MPI_COMM_WORLD);
       MPI_Bcast(&g, 1, MPI_INT, 0, MPI_COMM_WORLD);
       MPI_Bcast(&h, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  }
+  
   //Suppose we don't know the secret key. Use all the ranks to try and find it in parallel
-    printf("Using %d processes to find the secret key...\n", size);
+  if (rank ==0){  
+  printf("Using %d processes to find the secret key...\n", size);
+  }
   /*Q3.2 We want to loop through values i=0 .. p-2
      determine start and end values so this loop is 
      distributed amounst the MPI ranks  */
   unsigned int N = p-1; //total loop size
   unsigned int start, end, startTime, endTime;
-  
+
   if (rank == 0){
   startTime = MPI_Wtime();
   for (int i = 0; i < N-1; i++)
@@ -68,17 +72,26 @@ int main (int argc, char **argv) {
      }
      end = start + N; // originial end = start + N
   }
-  } 
+  }
   //loop through the values from 'start' to 'end'
   for (unsigned int i=start;i<end;i++) {
-    if (modExp(g,i+1,p)==h)
+    if (modExp(g,i+1,p)==h){
       printf("Secret key found! x = %u \n", i+1);
-  }
+      break;
+} 
+}
   endTime = MPI_Wtime();
-  printf("The total time it took to run the for loop: %d.\n", endTime - startTime);
+    
+  double timeElapsed = endTime - startTime;
+  double throughPut = (double)N/timeElapsed;
+  //For some reason the time elapsed when first ran registers as 0, throwing infinity when throughput is trying to be found. 
+  printf("The total time it took to run the for loop: %g.\n", timeElapsed);
   printf("Start time: %d.\n", startTime);
   printf("End time: %d.\n", endTime);
-  fflush(stdout);  
+  printf("Throughput: %g.\n", throughPut);
+ 
+
+  fflush(stdout); 
   MPI_Finalize();
 
   return 0;
